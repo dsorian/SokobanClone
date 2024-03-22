511881fb203a0a60edff94e4b0e44f34c400f00a
dsorian <dsorian.dev@gmail.com>
2024-03-22 13:41:53 +0100

    Empezando a probar cosas
    No sé si un tilemap me servirá.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
@@ -25,6 +26,10 @@ public class GameManager : MonoBehaviour
             // Colocar un tile en las coordenadas del Tilemap
             tilemap.SetTile(tilePos, tile);
         }
+    }
+
+    public void ParedSeleccionada(){
+        Debug.Log("Pared seleccionada.");
     }
 
 }
